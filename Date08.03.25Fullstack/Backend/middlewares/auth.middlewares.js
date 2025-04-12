b20c1a140659e18ce988c340762429cb7e24e581
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-04-12 21:04:14 +0530

    frontend started

--- Date08.03.25Fullstack/Backend/middlewares/auth.middlewares.js
+++ Date08.03.25Fullstack/Backend/middlewares/auth.middlewares.js
@@ -5,15 +5,15 @@ const verifyJWT = async (req, res, next) => {
         const token = req.cookies?.token
         
         if(!token){
-            res.status(400).json({
+            return res.status(400).json({
                 message: "please login" 
             })
         }
-        const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decodedToken
         next()
     } catch (error) {
-        res.status(400).json({
+        return res.status(400).json({
             message: "user not logged in"
         })
     }
